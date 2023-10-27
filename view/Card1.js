fdc5f98104a2da058877ee7e3edc1668164663db
Juanco <juanffo874@gmail.com>
2023-10-26 22:57:13 -0600

    Creacion de las vistas de la cards 1 y 2, pero sin estilos

---
+++ view/Card1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Card1 = () => {
+  return (
+    <View>
+      <Text>Desde la primera card</Text>
+    </View>
+  )
+}
+
+export default Card1
---
+++ view/Card2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Card2 = () => {
+  return (
+    <View>
+      <Text>Desde la segunda card</Text>
+    </View>
+  )
+}
+
+export default Card2
